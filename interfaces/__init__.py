6f16ed1af0c8c7d2116e075ece6ac415ffbc9e16
ElectroUnity <ElectroUnity865@outlook.com>
2024-03-24 10:42:01 +0800

    Fix a few bugs

--- interfaces/__init__.py
+++ interfaces/__init__.py
@@ -1,4 +1,4 @@
 from .interface_add import InterfaceAdd
 from .interface_solve import InterfaceSolve
 from .interface_help import InterfaceHelp
-from .setting_interface import SettingInterface
+from .interface_settings import SettingInterface
--- interfaces/config.py
+++ interfaces/config.py
@@ -1,20 +1,10 @@
 # coding:utf-8
 
-from qfluentwidgets import (qconfig, QConfig, ConfigItem, OptionsConfigItem, BoolValidator,
-                            ColorConfigItem, OptionsValidator, RangeConfigItem, RangeValidator)
+from qfluentwidgets import qconfig, QConfig
 
 
 class Config(QConfig):
     """ Config of application """
-
-    # main window
-    minimizeToTray = ConfigItem(
-        "MainWindow", "MinimizeToTray", True, BoolValidator())
-    playBarColor = ColorConfigItem("MainWindow", "PlayBarColor", "#225C7F")
-    recentPlaysNumber = RangeConfigItem(
-        "MainWindow", "RecentPlayNumbers", 300, RangeValidator(10, 300))
-    dpiScale = OptionsConfigItem(
-        "MainWindow", "DpiScale", "Auto", OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]), restart=True)
 
 
 cfg = Config()
