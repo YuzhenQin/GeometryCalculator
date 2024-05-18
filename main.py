dc16475ced1e63644c1f937877e0f11074651264
ElectroUnity <ElectroUnity865@outlook.com>
2024-05-18 14:48:43 +0800

    Update index.html

--- main.py
+++ main.py
@@ -137,9 +137,6 @@ class Window(FluentWindow):
 if __name__ == '__main__':
     # 启用高分辨率缩放 enable hidpi scale
     QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.Ceil)
-    # 主题默认跟随系统设置
-    # setTheme(theme=Theme.AUTO)
-
     app = QApplication(sys.argv)
     # 国际化
     # 为啥translator不生效啊，我不理解啊啊啊啊啊
